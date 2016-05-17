5ba91a229c3434662a6c54ff9f92f45f326cac30
Igor Mandello <igormandello@gmail.com>
2016-05-17 00:48:11 -0300

    Mudanças no script, index e adicionado arquivos
    Adicionado biblioteca jQuery
    Mudanças no script da página
    Colocado ícones para o menu de navegação
    
    Mudanças no index:
    Feito rodapé com direitos autorais de ícones
    Começado menu de navegação

---
+++ js/script.js
@@ -0,0 +1,45 @@
+
+function carregar(){
+	$("#arquimedes").hide();
+	$("#painel").hide();
+
+	// rodarSlides();
+
+	$("#bemvindo").click( function(){
+        var alturaTela = $(document).height();
+        var larguraTela = $(window).width();
+     
+        //colocando o fundo preto
+        $('#mascara').css({'width':larguraTela,'height':alturaTela});
+        $('#mascara').fadeTo(500,1); 
+        $('#mascara').fadeTo("slow",0.8);
+ 
+        var left = ($(window).width() /2) - ( $("#painel").width() / 2 );
+        var top = ($(window).height() / 2) - ( $("#painel").height() / 2 );
+     
+        $("#painel").css({top: top,left: left});
+        $("#painel").show();   
+    });
+ 
+    $("#mascara").click( function(){
+        $(this).hide();
+        $("#painel").hide();
+    });	
+
+};
+
+function rodarSlides(){
+	switch (n) {
+		case 1:
+			$("#bemvindo").show("slide", { direction: "left" }, 600);
+			$("#arquimedes").show("slide", { direction: "left" }, 600);
+			n = 2;
+			break;
+		case 2:
+			$("#arquimedes").SlifeLeft();
+			$("#bemvindo").slideLeft();
+			n = 1;
+			break;
+	}
+	setTimeout(rodarSlides, 6000);
+}
