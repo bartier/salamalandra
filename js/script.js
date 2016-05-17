123864986c58bde23aaf038634864aedeb812943
Vitor Bartier <vitorbartier@hotmail.com>
2016-05-17 09:26:05 -0300

    Corrigido ae

--- js/script.js
+++ js/script.js
@@ -6,12 +6,14 @@ function carregar(){
 	// rodarSlides();
 
 	$("#bemvindo").click( function(){
-        var alturaTela = $(document).height();
+		$("<div id='mascara'></div>");
+
+		//achar a altura atual
+        var alturaTela = $(document).height() + $(document).scrollTop();
         var larguraTela = $(window).width();
-     
+     	
         //colocando o fundo preto
         $('#mascara').css({'width':larguraTela,'height':alturaTela});
-        $('#mascara').fadeTo(500,1); 
         $('#mascara').fadeTo("slow",0.8);
  
         var left = ($(window).width() /2) - ( $("#painel").width() / 2 );
