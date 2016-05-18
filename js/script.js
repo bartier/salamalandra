3f5c12e3d0302c5fb9dafea9497f0e837b9dc73b
Igor Mandello <igormandello@gmail.com>
2016-05-18 00:14:04 -0300

    Finalmente os slider deu certo
    Botei uns icone pra nav também é nóis bart
    agora ta bonito
    Visual D++++++++++++++++
    C# > Delphi
    Delphi > Design

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
+	var n = 1;
+	var m = 1;
+
 function carregar() {
+
 	$("#arquimedes").hide();
 	$("#painel").hide();
 
@@ -25,20 +29,62 @@ function carregar() {
         $("#painel").hide();
     });	
 
+    rodarSlides();
+    rodarTextos();
 };
 
 function rodarSlides(){
 	switch (n) {
 		case 1:
-			$("#bemvindo").show("slide", { direction: "left" }, 600);
-			$("#arquimedes").show("slide", { direction: "left" }, 600);
+			$("#4").hide("normal", function(){
+			$("#1").show("normal"); });
 			n = 2;
 			break;
 		case 2:
-			$("#arquimedes").SlifeLeft();
-			$("#bemvindo").slideLeft();
+			$("#1").hide("normal", function(){
+			$("#2").show("normal"); });
+			n = 3;
+			break;
+		case 3:
+			$("#2").hide("normal", function(){
+			$("#3").show("normal"); });
+			n = 4;
+			break;
+		case 4:
+			$("#3").hide("normal", function(){
+			$("#4").show("normal"); });
 			n = 1;
 			break;
 	}
+
 	setTimeout(rodarSlides, 6000);
 };
+
+function rodarTextos() {
+	switch (m) {
+		case 1:
+			$("#textoArquimedes").animate({left:'35%',opacity:'1'},1000, function(){
+			$("#textoArquimedes2").animate({left:'75%',opacity:'1'},1000) });
+			m = 2;
+			break;
+		case 2:
+			$("#textoArquimedes").animate({left:'0%',opacity:'0'},800, function() {
+			$("#textoArquimedes2").animate({left:'0%',opacity:'0'},1000, function() { 
+			$("#textoArquimedes").css({left:'100%'});
+			$("#textoArquimedes2").css({left:'100%'});
+			}); });
+			m = 3;
+			break;
+		case 3:
+			$("#propaganda").animate({left:'30%',opacity:'1'},1000);
+			m = 4;
+			break;
+		case 4:
+			$("#propaganda").animate({left:'100%',opacity:'0'},800, function() {
+			$("#propaganda").css({left:'0%'}); });
+			m = 1;
+			break;
+	}
+
+	setTimeout(rodarTextos, 6000);
+}
